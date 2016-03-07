78c1f2f74a676b2243fd623aad92cad4e93f0d1b
Nalin_C <nalin.chhibber@comprotechnologies.com>
2016-03-07 18:22:01 +0530

    add sample md

--- public/script.js
+++ public/script.js
@@ -7,5 +7,15 @@
 		$('#preview').html(html_content)      
 	}		
 
+/*	function parseMD(){
+		var md = new Remarkable();
+
+		md_content = $('#md').val(),
+		html_content = md.render(md_content);
+
+		$('#preview').html(html_content)
+	}*/
+
+
 	this.parseMD = parseMD
 })();
